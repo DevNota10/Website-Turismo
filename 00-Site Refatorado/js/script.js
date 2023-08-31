2673098983a7ff3f80abc512fd86af4bdba149ef
Kleber <dev.nota10.k@gmail.com>
2023-08-30 21:47:02 -0300

    update

--- 00-Site Refatorado/js/script.js
+++ 00-Site Refatorado/js/script.js
@@ -76,10 +76,41 @@ subMenu.classList.toggle("subMenuActivo");
 }
 
 // Animation of Section About 
-// function animateSecAbout(){
-//   const txtAboutUs = document.querySelector(".text-about-us");
+function animateSecAbout(){
+  // Animation text about: 
+  const txtAboutUs = document.querySelectorAll(".text-about-us");
+  txtAboutUs.forEach((target)=>{
+    target.addEventListener("mouseover",()=>{
+       if('mouseover'){
+      target.style.maxWidth='90%';
 
-// }
+        // INDIVIDUALIZAR EFEITO HOUVER DA imageAuthor ****
+      const imageAuthor = document.querySelectorAll('.bg_img > div');
+      imageAuthor.forEach((i)=>{
+        i.style.transform ='rotate(0deg)'
+      })
+      return false;
+     };
+
+    });
+    target.addEventListener('mouseout',()=>{
+      target.style.maxWidth='450px';       
+    });
+  })
+
+  // animation of img box about:
+  const bxImgAbout = document.querySelectorAll(".bx-img-about");
+  bxImgAbout.forEach((target)=>{
+    target.addEventListener("mouseover",()=>{
+      target.style.width='90%';
+    });
+
+    target.addEventListener('mouseout',()=>{
+      target.style.width='100px';       
+    });
+  })
+}
+animateSecAbout()
 
 
 // Text About the place:
@@ -114,7 +145,6 @@ subMenu.classList.toggle("subMenuActivo");
      getTxtPart2.style.opacity='1'
      getTxtPart2.style.transform='rotate(0deg)'
     
-    
      x.style.display='none'
      
 
@@ -126,7 +156,6 @@ subMenu.classList.toggle("subMenuActivo");
      closeExpandText(x,getTxtPart2);
       });
     });
-
   };
 
    closeExpandText = (x,getTxtPart2) =>{
@@ -140,16 +169,265 @@ subMenu.classList.toggle("subMenuActivo");
         getTxtPart2.style.opacity='0';
         getTxtPart2.style.transform='rotate(10deg)';
   
-      })
+      });
 
-    })
+    });
+   };
+
+    // ClickSidenav:
+    const sidenavLink = document.querySelectorAll(".sidenav li");
+springtime = ()=>{
+      
+sidenavLink.forEach((li,idx)=>{
+// Seasons Springtime:
+  li.addEventListener('click', ()=>{
+  let curIdx = document.querySelectorAll(".sidenav")[0].parentElement;
+  
+  
+    switch(idx){
+        case 0:  
+        curIdx.style.backgroundImage= "url(./landscape/flores.jpg)"
+        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 1</h2>`
+        break;
+        case 1:  
+        curIdx.style.background=' #ccc'
+        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 2</h2>`
+        break;
+        case 2: 
+        curIdx.style.background=' #ff0000'
+        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 3</h2>`
+        break;
+        case 3: 
+        curIdx.style.background=' #ccc'
+        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 4</h2>`
+        break;
+        case 4: 
+        curIdx.style.background=' #ff0000'
+        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 5</h2>`
+        break;
+        case 5: 
+        curIdx.style.background=' #ccc'
+        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 6</h2>`
+        break;
+        case 6: 
+        curIdx.style.background=' #ff0000'
+        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 7</h2>`
+        break;
+        case 7: 
+        curIdx.style.background=' #ccc'
+        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 8</h2>`
+        break;
+        case 8: 
+        curIdx.style.background=' #ff0000'
+        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 9</h2>`
+        break;
+        case 9: 
+        curIdx.style.background=' #ccc'
+        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 10</h2>`
+        break;
+
+    };
+  
+  });
+        
+    });
+};
+                          // ------- fim----------
+springtime();
+
+summer = ()=>   {
+    // Seasons Summer:;
+    sidenavLink.forEach((li,idx)=>{
+  li.addEventListener('click', ()=>{
+    let curIdx = document.querySelectorAll(".sidenav")[1].parentElement;
 
 
+    switch(idx){
+        case 10:  
+        curIdx.style.backgroundImage= "url(./landscape/beach-1838501_1920.jpg)"
+        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 1</h2>`;
+        break;
+        case 11:  
+        curIdx.style.background=' #ccc'
+        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 2</h2>`;
+        break;
+        case 12: 
+        curIdx.style.background=' #ff0000'
+        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 3</h2>`;
+        break;
+        case 13: 
+        curIdx.style.background=' #ccc'
+        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 4</h2>`;
+        break;
+        case 14: 
+        curIdx.style.background=' #ff0000'
+        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 5</h2>`;
+        break;
+        case 15: 
+        curIdx.style.background=' #ccc'
+        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 6</h2>`;
+        break;
+        case 16: 
+        curIdx.style.background=' #ff0000'
+        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 7</h2>`;
+        break;
+        case 17: 
+        curIdx.style.background=' #ccc'
+        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 8</h2>`;
+        break;
+        case 18: 
+        curIdx.style.background=' #ff0000'
+        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 9</h2>`;
+        break;
+        case 19: 
+        curIdx.style.background=' #ccc'
+        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 10</h2>`;
+        break;
+
+    };
+  
+  });
+        
+  });
+
+  };
+  summer();
+                              // ------- fim----------
+
+// Seasons Fall:
+fall = () =>{
+  sidenavLink.forEach((li,idx)=>{
+    li.addEventListener('click', ()=>{
+      let curIdx = document.querySelectorAll(".sidenav")[2].parentElement;
+
+      switch(idx){
+          case 20:  
+          curIdx.style.backgroundImage= "url(./landscape/beach-1838501_1920.jpg)";
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 1</h2>`;
+          break;
+          case 21:  
+          curIdx.style.background=' #ccc'
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 2</h2>`;
+          break;
+          case 22: 
+          curIdx.style.background=' #ff0000'
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 3</h2>`;
+          break;
+          case 23: 
+          curIdx.style.background=' #ccc'
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 4</h2>`;
+          break;
+          case 24: 
+          curIdx.style.background=' #ff0000'
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 5</h2>`;
+          break;
+          case 25: 
+          curIdx.style.background=' #ccc'
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 6</h2>`;
+          break;
+          case 26: 
+          curIdx.style.background=' #ff0000'
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 7</h2>`;
+          break;
+          case 27: 
+          curIdx.style.background=' #ccc'
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 8</h2>`;
+          break;
+          case 28: 
+          curIdx.style.background=' #ff0000'
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 9</h2>`;
+          break;
+          case 29: 
+          curIdx.style.background=' #ccc'
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 10</h2>`;
+          break;
+
+      };
+
+    });
+          
+  });
+
+};
+
+fall()
+           // ------- fim----------
+
+           // Seasons Winter:
+
+winter = ()=>{
+
+    sidenavLink.forEach((li,idx)=>{
+        // Seasons Springtime:
+    li.addEventListener('click', ()=>{
+    let curIdx = document.querySelectorAll(".sidenav")[3].parentElement;
+
+
+      switch(idx){
+          case 30:  
+          curIdx.style.backgroundImage= "url(./landscape/temple-5909803_1920.jpg)"
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 1</h2>`;
+          break;
+          case 31:  
+          curIdx.style.background=' #ccc'
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 2</h2>`;
+          break;
+          case 32: 
+          curIdx.style.background=' #ff0000'
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 3</h2>`;
+          break;
+          case 33: 
+          curIdx.style.background=' #ccc'
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 4</h2>`;
+          break;
+          case 34: 
+          curIdx.style.background=' #ff0000'
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 5</h2>`;
+          break;
+          case 35: 
+          curIdx.style.background=' #ccc'
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 6</h2>`;
+          break;
+          case 36: 
+          curIdx.style.background=' #ff0000'
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 7</h2>`;
+          break;
+          case 37: 
+          curIdx.style.background=' #ccc'
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 8</h2>`;
+          break;
+          case 38: 
+          curIdx.style.background=' #ff0000'
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 9</h2>`;
+          break;
+          case 39: 
+          curIdx.style.background=' #ccc'
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 10</h2>`;
+          break;
+
+      };
+
+    });
+
+  });
+};
+winter();
+          
+
+
+
+        
+
+
+      
+       
+   
+      
+   
+  
+    
 
     
-  
-   }
-
 
 
 
