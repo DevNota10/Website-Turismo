bd027f2f9f0f7219cb9f6e1e5daabc04bcf76102
Kleber <dev.nota10.k@gmail.com>
2023-09-01 19:15:33 -0300

    Alterações de txt ao clicar na li

--- 00-Site Refatorado/js/script.js
+++ 00-Site Refatorado/js/script.js
@@ -182,16 +182,77 @@ sidenavLink.forEach((li,idx)=>{
 // Seasons Springtime:
   li.addEventListener('click', ()=>{
   let curIdx = document.querySelectorAll(".sidenav")[0].parentElement;
-  
-  
+
+    
+// Alterando textos sobre as cidades:
+function changeTxtAboutThePlace (){
+  const getContentBx = document.querySelectorAll(".text-about-the-place")
+  // let title = getContentBx[0]
+  let txt_1 =  getContentBx[0].children[2].innerHTML = allSpringTxt.titulo_02;
+  let txt_2 =  txt_1.nextElementSibling; 
+}
+
+  let title = document.querySelector(".text-about-the-place > h1");
+
+      const  allSpringTxt = {
+        titulo_02: title.innerHTML = `Lugar 02`,
+        txt_li_02: `<p> Texto Li 02 </p>`,
+      }
+          
+      // titulo_02: title.innerHTML = `Lugar 02`,
+      // txt_li_02: `<p> Texto Li 02 </p>`,
+      // getContent: (titulo_02, txt_li_02)=>{
+      //   return titulo_02 + txt_li_02
+    
+      // li_03 =  {
+      //     titulo: title.innerHTML = `Lugar 03`,
+      //     txt_li_03: `<p> Texto Li 03 </p>`
+      //   },
+
+        // returnTxt: function (){
+        //    return this.AllTxt
+        // }
+        // {
+        //   txt_li_04: `<p> Texto Li 04 </p>`
+        // },
+        // {
+        //   titulo: title.innerHTML = `Lugar 05`,
+        //   txt_li_05: `<p> Texto Li 05 </p>`
+        // },
+        // {
+        //   titulo: title.innerHTML = `Lugar 06`,
+        //   txt_li_05: `<p> Texto Li 06 </p>`
+        // },
+        // {
+        //   titulo: title.innerHTML = `Lugar 07`,
+        //   txt_li_07: `<p> Texto Li 07 </p>`
+        // },
+        // {
+        //   titulo: title.innerHTML = `Lugar 08`,
+        //   txt_li_08: `<p> Texto Li 08 </p>`
+        // },
+        // {
+        //   titulo: title.innerHTML = `Lugar 09`,
+        //   txt_li_09: `<p> Texto Li 09 </p>`
+        // }
+      
+          
+
     switch(idx){
-        case 0:  
-        curIdx.style.backgroundImage= "url(./landscape/flores.jpg)"
-        curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 1</h2>`
+
+        case 0:
+          curIdx.style.backgroundImage= "url(./landscape/flores.jpg)"
+          curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 1</h2>`
+
         break;
         case 1:  
         curIdx.style.background=' #ccc'
         curIdx.getElementsByClassName("idx-place")[0].innerHTML= `<h2>Lugar 2</h2>`
+      
+        changeTxtAboutThePlace(AllSpringTxt[0]);
+        AllSpringTxt[0].innerHTML= li_02.
+      
+
         break;
         case 2: 
         curIdx.style.background=' #ff0000'
@@ -231,9 +292,18 @@ sidenavLink.forEach((li,idx)=>{
   });
         
     });
+
+    
 };
                           // ------- fim----------
 springtime();
+
+
+
+
+
+
+
 
 summer = ()=>   {
     // Seasons Summer:;
